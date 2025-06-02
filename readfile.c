2c6f981d15e0f887cd16a6bbc82f25c2bf3f3478
Austin-Landschoot <austin.landschoot@students.coderva.org>
2025-06-01 20:35:22 -0400

    Create readfile.c

---
+++ readfile.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    if (argc == 1) {
+        printf("No file provided.");
+        return 1;
+    }
+    else if (argc == 2) {
+        FILE *file = fopen(argv[1], "r");
+        if (file != NULL) {
+            int ch;
+            while ((ch = fgetc(file)) != EOF) {
+                putchar(ch);
+            }
+            return 0;
+        }
+        else {
+            perror("File not found");
+            return 1;
+        }
+    }
+    else {
+        printf("Too many arguments provided.");
+        return 1;
+    }
+}
